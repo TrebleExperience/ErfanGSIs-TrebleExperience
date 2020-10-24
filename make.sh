01b2f96b2925e07421bb20ab2672608a68cb827d
Enzo Vieira | YukoSky <daffetyxd@gmail.com>
2020-10-24 20:44:35 -0300

    [STAGING] Minor fix for debloat act
    Signed-off-by: Enzo Vieira | YukoSky <daffetyxd@gmail.com>

--- make.sh
+++ make.sh
@@ -145,7 +145,7 @@ if [[ ! -f "$systemdir/system/lib64/libandroid.so" ]]; then
 fi
 
 # Debloat
-if [ -f "$romsdir/$sourcever/$romtype/debloat.sh" || -f "$romsdir/$sourcever/$romtype/$romtypename/debloat.sh" ]; then
+if [[ -f "$romsdir/$sourcever/$romtype/debloat.sh" ]]; then
     echo "-> De-bloating"
     $romsdir/$sourcever/$romtype/debloat.sh "$systemdir/system" 2>/dev/null
     $romsdir/$sourcever/$romtype/$romtypename/debloat.sh "$systemdir/system" 2>/dev/null
