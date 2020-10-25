55008d603285080175598afe83080479f23f868a
Enzo Vieira | YukoSky <daffetyxd@gmail.com>
2020-10-25 13:19:42 -0300

    [STAGING] Bring-up --debloat|-d option
    Signed-off-by: Enzo Vieira | YukoSky <daffetyxd@gmail.com>

--- make.sh
+++ make.sh
@@ -145,7 +145,7 @@ if [[ ! -f "$systemdir/system/lib64/libandroid.so" ]]; then
 fi
 
 # Debloat
-if [[ -f "$romsdir/$sourcever/$romtype/debloat.sh" ]]; then
+if [$DEBLOAT == true]; then
     echo "-> De-bloating"
     $romsdir/$sourcever/$romtype/debloat.sh "$systemdir/system" 2>/dev/null
     $romsdir/$sourcever/$romtype/$romtypename/debloat.sh "$systemdir/system" 2>/dev/null
