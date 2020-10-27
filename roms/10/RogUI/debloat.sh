ecad88f4b6138c522c9c90d00185399a9942a0c7
Enzo Vieira | YukoSky <daffetyxd@gmail.com>
2020-10-27 19:54:45 -0300

    [TMP] Get debloat from pie to Q (RogUI 10)
    Signed-off-by: Enzo Vieira | YukoSky <daffetyxd@gmail.com>

--- roms/10/RogUI/debloat.sh
+++ roms/10/RogUI/debloat.sh
@@ -27,3 +27,33 @@ rm -rf $1/product/app/Youtube
 rm -rf $1/product/priv-app/Turbo
 rm -rf $1/product/priv-app/Velvet
 rm -rf $1/product/priv-app/Wellbeing
+
+# Debloat from Pie
+rm -rf $1/priv-app/AsusGallery
+rm -rf $1/priv-app/SmartReading
+rm -rf $1/priv-app/AsusDataTransfer
+rm -rf $1/priv-app/DisplayLink
+rm -rf $1/priv-app/GameModeLiveWallpaper
+rm -rf $1/priv-app/YandexBrowser
+rm -rf $1/priv-app/YandexApp
+rm -rf $1/priv-app/AsusCamera
+rm -rf $1/app/Videos
+rm -rf $1/app/GooglePay
+rm -rf $1/app/Music2
+rm -rf $1/app/Asphalt9
+rm -rf $1/priv-app/AsusDataTransfer
+rm -rf $1/app/BRApps
+rm -rf $1/priv-app/AsusCamera
+rm -rf $1/app/Netflixstubplus
+rm -rf $1/app/NetflixActivation
+rm -rf $1/priv-app/WeatherTime
+rm -rf $1/priv-app/FacebookInstaller
+rm -rf $1/priv-app/FacebookNotificationServices
+rm -rf $1/priv-app/MyASUS
+rm -rf $1/priv-app/AsusLocalBackup
+rm -rf $1/app/AtokIME
+rm -rf $1/app/JakartaBaca
+rm -rf $1/app/talkback
+rm -rf $1/app/mangaDeals
+rm -rf $1/priv-app/LogUploader
+rm -rf $1/priv-app/ZenUIHelp
