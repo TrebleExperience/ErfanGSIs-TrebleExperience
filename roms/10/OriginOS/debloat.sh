9859685bfac2f60edad3742a5583cedeb8fbc49f
Enzo Vieira | YukoSky <daffetyxd@gmail.com>
2020-12-07 23:01:56 -0300

    [STAGING] Bring-up OriginOS/FuntouchOS
    Signed-off-by: Enzo Vieira | YukoSky <daffetyxd@gmail.com>

---
+++ roms/10/OriginOS/debloat.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+systempath=$1
+thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
+
+# MIUI + FOS
+rm -rf $1/priv-app/Backup
+rm -rf $1/priv-app/Browser
+rm -rf $1/priv-app/Calendar
+rm -rf $1/priv-app/MiuiScanner
+rm -rf $1/priv-app/Updater
+rm -rf $1/priv-app/Velvet
+rm -rf $1/priv-app/Weather
+rm -rf $1/priv-app/WeatherProvider
+rm -rf $1/app/VoiceAssist
+rm -rf $1/app/NexyPay
+rm -rf $1/priv-app/PersonalAssistant
+rm -rf $1/priv-app/Music
+rm -rf $1/data-app/MiShop
+rm -rf $1/data-app/XiaoAiSpeechEngine
+rm -rf $1/data-app/Email
+rm -rf $1/data-app/Weather
+rm -rf $1/data-app/CleanMaster
+rm -rf $1/app/AnalyticsCore
+rm -rf $1/app/BasicDreams
+rm -rf $1/app/BugReport
+rm -rf $1/app/Drive
+rm -rf $1/app/Duo
+rm -rf $1/app/Gmail2
+rm -rf $1/app/Maps
+rm -rf $1/app/MiPicks
+rm -rf $1/app/MiuiAudioMonitor
+rm -rf $1/app/MiuiVideoPlayer
+rm -rf $1/app/Music2
+rm -rf $1/app/Photos
+rm -rf $1/app/SystemAppUpdater
+rm -rf $1/app/TranslationService
+rm -rf $1/app/Videos
+rm -rf $1/app/YouDaoEngine
+rm -rf $1/app/YouTube
+rm -rf $1/app/XiaomiServiceFramework
+rm -rf $1/priv-app/Provision
+rm -rf $1/app/facebook-appmanager
+rm -rf $1/app/talkback
+rm -rf $1/priv-app/Turbo
+rm -rf $1/priv-app/YellowPage
+rm -rf $1/priv-app/facebook-installer
+rm -rf $1/priv-app/facebook-services
+rm -rf $1/app/Lens
+rm -rf $1/product/priv-app/Velvet
+rm -rf $1/product/priv-app/AndroidAutoStub
+rm -rf $1/product/priv-app/HotwordEnrollmentOKGoogleHEXAGON
+rm -rf $1/product/priv-app/Turbo
+rm -rf $1/product/priv-app/HotwordEnrollmentXGoogleHEXAGON
+rm -rf $1/product/app/Gmail2
+rm -rf $1/product/app/GoogleTTS
+rm -rf $1/product/app/Maps
+rm -rf $1/product/app/YouTube
+rm -rf $1/product/app/talkback
+rm -rf $1/app/AnalyticsCore
+rm -rf $1/priv-app/GoogleDocumentsUIPrebuilt
+rm -rf $1/data-app/MiCreditInStub
+rm -rf $1/product/app/CalendarGoogle
+rm -rf $1/product/app/GoogleContacts
+rm -rf $1/product/app/GoogleOne
+rm -rf $1/recovery-from-boot.p
+rm -rf $1/product/priv-app/Wellbeing
+
+# More
+
+rm -rf $1/priv-app/Maps
+rm -rf $1/priv-app/Velvet
+rm -rf $1/priv-app/VsCamera
+rm -rf $1/app/YouTube
+rm -rf $1/app/Photos
+rm -rf $1/app/Messages
+rm -rf $1/app/Drive
+rm -rf $1/app/Gmail2
+rm -rf $1/app/Translate
+rm -rf $1/app/Duo
+rm -rf $1/app/Keep
+rm -rf $1/app/GoogleTTS
+rm -rf $1/app/talkback
+rm -rf $1/app/Maps
+rm -rf $1/app/Velvet
+rm -rf $1/app/BBKCloud
+rm -rf $1/app/BBKFMRadio
+rm -rf $1/app/BBKLOG
+rm -rf $1/app/BBKWeatherProvider
+rm -rf $1/app/Backup
+rm -rf $1/app/Chrome
+rm -rf $1/app/Coral
+rm -rf $1/app/Drive
+rm -rf $1/app/Duo
+rm -rf $1/app/EWarranty
+rm -rf $1/app/Gmail2
+rm -rf $1/app/Maps
+rm -rf $1/app/Music2
+rm -rf $1/app/Photos
+rm -rf $1/app/VLife_vivo
+rm -rf $1/app/Video*
+rm -rf $1/app/VivoAssistant
+rm -rf $1/app/VideoEditor
+rm -rf $1/app/VideoPlayer
+rm -rf $1/app/VLife_vivo
+rm -rf $1/app/VivoDreamWeatherApp
+rm -rf $1/app/VivoSmartMultiWindow
+rm -rf $1/app/VivoTips
+rm -rf $1/app/VivoUnionPayEx
+rm -rf $1/app/VivoWebsite
+rm -rf $1/app/YouTube
+rm -rf $1/app/facebook-appmanager
+rm -rf $1/app/vivoCollage
+rm -rf $1/app/vivoDemoVideo
+rm -rf $1/app/Youtube
+rm -rf $1/app/VivoCamera
+
+rm -rf $1/priv-app/Email
+rm -rf $1/priv-app/FloatingCamera
+rm -rf $1/priv-app/GoogleRestore
+rm -rf $1/priv-app/SetupWizard
+rm -rf $1/priv-app/VivoBrowser
+rm -rf $1/priv-app/VivoAppStoreEX
+rm -rf $1/priv-app/facebook-installer
+rm -rf $1/priv-app/facebook-services
+rm -rf $1/priv-app/GmsCore
+
+
+rm -rf $1/app/AppFilter
+rm -rf $1/app/BBKAccount
+
+# mifavour
+rm -rf $1/priv-app/Maps
+rm -rf $1/priv-app/Velvet
+rm -rf $1/priv-app/VsCamera
+rm -rf $1/app/YouTube
+rm -rf $1/app/Photos
+rm -rf $1/app/Messages
+rm -rf $1/app/Drive
+rm -rf $1/app/Gmail2
+rm -rf $1/app/Translate
+rm -rf $1/app/Duo
+rm -rf $1/app/Keep
+rm -rf $1/app/GoogleTTS
+rm -rf $1/app/talkback
+rm -rf $1/app/Maps
+rm -rf $1/app/Velvet
