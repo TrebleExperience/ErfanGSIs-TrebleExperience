f5c37a8329eb2bded6736f895af712ea15c73d0f
Enzo Vieira | YukoSky <daffetyxd@gmail.com>
2020-12-07 22:31:09 -0300

    roms: 10: DU/AOS/PE/Evox: Drop
    Signed-off-by: Enzo Vieira | YukoSky <daffetyxd@gmail.com>

--- roms/10/EvolutionX/make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-systempath=$1
-thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
-
-echo "ro.setupwizard.mode=DISABLED" >> $1/etc/prop.default
-echo "qemu.hw.mainkeys=0" >> $1/etc/prop.default
