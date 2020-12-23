374ba19576cab180aeb3b0cb368295f38d16b2d4
Enzo Vieira | YukoSky <daffetyxd@gmail.com>
2020-12-23 20:19:15 -0300

    roms: 11: Drop OneUI/MIUI/OxygenOS (bcz I'll make my own bring-up when I get my montana device again)
    Signed-off-by: Enzo Vieira | YukoSky <daffetyxd@gmail.com>

--- roms/11/MIUI/rw-system.add.sh
+++
@@ -1,5 +0,0 @@
-mount -o bind /mnt/phh/empty_dir /vendor/app || true
-
-if [ "$vndk" == 28 ]; then
-    mount -o bind /system/bin/wpa_supplicant /vendor/bin/hw/wpa_supplicant || true
-fi
--- roms/11/OxygenOS/rw-system.add.sh
+++
@@ -1,4 +0,0 @@
-if [ "$vndk" == 28 ]; then
-    mount -o bind /system/bin/wpa_supplicant /vendor/bin/hw/wpa_supplicant || true
-fi
-mount -o bind /mnt/phh/empty_dir /vendor/overlay || true
