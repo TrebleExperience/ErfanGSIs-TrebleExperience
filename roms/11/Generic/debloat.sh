d037639220b035aa6de5ff76bd02ac3e15837df4
Enzo Vieira | YukoSky <daffetyxd@gmail.com>
2020-12-25 18:27:58 -0300

    roms: 11: Generic: Fix debloat
    Signed-off-by: Enzo Vieira | YukoSky <daffetyxd@gmail.com>

---
+++ roms/11/Generic/debloat.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+systempath=$1
+thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
+
+rm -rf $1/product/app/CalendarGoogle
+rm -rf $1/product/app/GoogleContacts
+rm -rf $1/product/app/GoogleOne
+rm -rf $1/recovery-from-boot.p
+rm -rf $1/product/priv-app/Wellbeing
+rm -rf $1/priv-app/Turbo
+rm -rf $1/priv-app/YellowPage
+rm -rf $1/priv-app/facebook-installer
+rm -rf $1/priv-app/facebook-services
+rm -rf $1/app/Lens
+rm -rf $1/product/priv-app/Velvet
+rm -rf $1/product/priv-app/AndroidAutoStub
+rm -rf $1/product/priv-app/HotwordEnrollmentOKGoogleHEXAGON
+rm -rf $1/product/priv-app/Turbo
+rm -rf $1/product/priv-app/HotwordEnrollmentXGoogleHEXAGON
+rm -rf $1/product/app/Gmail2
+rm -rf $1/product/app/GoogleTTS
+rm -rf $1/product/app/Maps
+rm -rf $1/product/app/YouTube
+rm -rf $1/priv-app/Maps
+rm -rf $1/priv-app/Velvet
+rm -rf $1/app/YouTube
+rm -rf $1/app/Photos
+rm -rf $1/app/Messages
+rm -rf $1/app/Drive
+rm -rf $1/app/Gmail2
+rm -rf $1/app/Translate
+rm -rf $1/app/Duo
+rm -rf $1/app/Keep
+rm -rf $1/app/GoogleTTS
+rm -rf $1/app/talkback
+rm -rf $1/app/Maps
