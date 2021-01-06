74522cb9f9cde6ffdac11e9a89b9d87a2c12665d
Enzo Vieira | YukoSky <daffetyxd@gmail.com>
2021-01-05 21:29:59 -0300

    roms: 10: VOS: Fix debloat
    Signed-off-by: Enzo Vieira | YukoSky <daffetyxd@gmail.com>

--- roms/10/VOS/debloat.sh
+++ roms/10/VOS/debloat.sh
@@ -3,33 +3,27 @@
 systempath=$1
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
-# Debloat 1
-rm -rf $1/priv-app/Maps
-rm -rf $1/priv-app/Velvet
-rm -rf $1/priv-app/VsCamera
-rm -rf $1/app/YouTube
-rm -rf $1/app/Photos
-rm -rf $1/app/Messages
+# New debloat
+rm -rf $1/app/Duo
 rm -rf $1/app/Drive
 rm -rf $1/app/Gmail2
-rm -rf $1/app/Translate
-rm -rf $1/app/Duo
-rm -rf $1/app/Videos
-rm -rf $1/app/Music2
-rm -rf $1/app/Keep
-rm -rf $1/app/GoogleTTS
-rm -rf $1/app/talkback
 rm -rf $1/app/Maps
-rm -rf $1/app/Velvet
-
-# Debloat 2
+rm -rf $1/app/Photos
+rm -rf $1/app/Videos
+rm -rf $1/app/Youtube
+rm -rf $1/priv-app/Velvet
+rm -rf $1/priv-app/VsCamera
 rm -rf $1/product/app/CalculatorGoogle
 rm -rf $1/product/app/CalendarGoogle
-rm -rf $1/product/app/Drive
-rm -rf $1/product/app/Duo
-rm -rf $1/product/app/Gmail2
-rm -rf $1/product/app/Maps
-rm -rf $1/product/app/Youtube
-rm -rf $1/product/priv-app/Turbo
+rm -rf $1/product/app/DeskClockGoogle
+rm -rf $1/product/app/GoogleLocationHistory
+rm -rf $1/product/app/GoogleTTS
+rm -rf $1/product/app/Keep
+rm -rf $1/product/app/talkback
+rm -rf $1/product/app/WebViewGoogle
+rm -rf $1/product/app/YTMusic
+rm -rf $1/product/priv-app/AndroidAutoStub
+rm -rf $1/product/priv-app/GoogleFeedback
 rm -rf $1/product/priv-app/Velvet
+rm -rf $1/product/priv-app/Turbo
 rm -rf $1/product/priv-app/Wellbeing
