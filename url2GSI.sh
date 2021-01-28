23d241bffc89650a7ecf7cb2a4e998ee36d39dd5
Enzo Vieira | YukoSky <daffetyxd@gmail.com>
2021-01-27 21:13:28 -0300

    dynamic: Add india/reserve support
    Rename --opproduct to --oneplus
    
    Signed-off-by: Enzo Vieira | YukoSky <daffetyxd@gmail.com>

--- url2GSI.sh
+++ url2GSI.sh
@@ -147,7 +147,7 @@ if [ $MOUNTED == false ]; then
         URL="$ZIP_NAME"
     fi
     if [ "$DYNAMIC" == true ]; then
-       "$PROJECT_DIR"/dynamic.sh "$URL" --odm --product --ext --opproduct --overlays
+       "$PROJECT_DIR"/dynamic.sh "$URL" --odm --product --ext --oneplus --overlays
     elif [ $DYNAMIC == false ] ; then
        "$PROJECT_DIR"/zip2img.sh "$URL" "$PROJECT_DIR/working" || exit 1
     fi
