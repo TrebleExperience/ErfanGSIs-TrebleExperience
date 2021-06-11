44fac80f67be131d250a80efac14c3e8d2c103c7
Velosh <daffetyxd@gmail.com>
2021-06-11 11:10:14 -0300

    roms: 11: Flyme: Update debloat
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- roms/11/Flyme/debloat.sh
+++ roms/11/Flyme/debloat.sh
@@ -3,35 +3,42 @@
 systempath=$1
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
+# Delete recovery cache
 rm -rf $1/recovery-from-boot.p
-rm -rf $1/app/WebViewGoogle
-rm -rf $1/app/Drive
-rm -rf $1/app/Duo
-rm -rf $1/app/Maps
-rm -rf $1/app/YouTube
-rm -rf $1/app/talkback
-rm -rf $1/app/CalendarGoogle
-rm -rf $1/app/Photos
-rm -rf $1/app/GooglePay
-rm -rf $1/app/Music2
-rm -rf $1/app/Gmail2
-rm -rf $1/app/GoogleTTS
+
+# Delete some apps
 rm -rf $1/app/AppCenterIntl
 rm -rf $1/app/Camera
 rm -rf $1/app/IntlNews
 rm -rf $1/app/NotePaper
-rm -rf $1/app/Videos
-rm -rf $1/priv-app/Email
+rm -rf $1/priv-app/Browser
 rm -rf $1/priv-app/Music
 rm -rf $1/priv-app/Search
-rm -rf $1/priv-app/SoundRecorder
-rm -rf $1/priv-app/ToolBox
-rm -rf $1/priv-app/Velvet
+rm -rf $1/priv-app/FlymeDialer
+rm -rf $1/priv-app/FlymeLab
+rm -rf $1/priv-app/FlymeMusic
+rm -rf $1/priv-app/FlymeGallery
+rm -rf $1/priv-app/FlymeSoundRecorder
 rm -rf $1/priv-app/Video
-rm -rf $1/priv-app/wt_logcat
-rm -rf $1/app/MzUpdate*
+
+# Delete APKs from MzApp
+rm -rf $1/MzApp/Calculator
+rm -rf $1/MzApp/Email
+rm -rf $1/MzApp/FlymeCalendar
+rm -rf $1/MzApp/GoogleCalendarSyncAdapter
 rm -rf $1/MzApp/Game*
 rm -rf $1/MzApp/Life
 rm -rf $1/MzApp/MzStore
 rm -rf $1/MzApp/Reader
-rm -rf $1/MzApp/VideoClips
+rm -rf $1/MzApp/ToolBox
+rm -rf $1/MzApp/Weather
+
+# Delete MzUpdate
+rm -rf $1/app/MzUpdate
+
+# Delete some google apps (aka GApps)
+rm -rf $1/system_ext/priv-app/GoogleOneTimeInitializer
+rm -rf $1/system_ext/priv-app/GoogleServicesFramework
+
+# Delete qcril
+rm -rf $1/system_ext/priv-app/qcrilmsgtunnel
