5cf1cf120c225589ca0e8cef5306efc932e6e3ec
Velosh <daffetyxd@gmail.com>
2021-06-11 10:20:29 -0300

    roms: 11: Flyme: Drop deprecated methods
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- roms/11/Flyme/make.sh
+++ roms/11/Flyme/make.sh
@@ -7,12 +7,10 @@ thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 echo "ro.bootprof.disable=1" >> $1/build.prop
 
 # Custom files
-cp -fpr $thispath/lib64/* $1/lib64/
 cp -fpr $thispath/erfan $1/
 cp -fpr $thispath/init/* $1/etc/init/
 cp -fpr $thispath/bin/* $1/bin/
 cp -fpr $thispath/overlay/* $1/product/overlay/
-cp -fpr $thispath/framework/* $1/framework/
 
 # hack bootprof
 sed -i "s|/sys/bootprof/bootprof|/system/erfan/bootprof|g" $1/lib/libsurfaceflinger.so
@@ -26,11 +24,3 @@ rm -rf $1/app/com.wolfsonmicro.ez2control
 
 # Append file_context
 cat $thispath/file_contexts >> $1/etc/selinux/plat_file_contexts
-
-# Fix Flyme Call
-"$thispath/../../../zip2img.sh" "$FIRMWARE_PATH" "$thispath/../../../working/" "-v"
-vendorpath="$thispath/../../../working/vendor"
-mkdir $vendorpath
-sudo mount $thispath/../../../working/vendor.img $vendorpath
-cp -frp $vendorpath/overlay/FrameworksResCommon.apk $1/product/overlay/VendorFrameworksResCommon.apk
-sudo umount $vendorpath
