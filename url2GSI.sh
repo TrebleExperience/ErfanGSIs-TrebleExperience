a8b496f2ed548e84ac051af236f2de455153f739
Velosh <daffetyxd@gmail.com>
2021-06-22 17:33:35 -0300

    url2GSI.sh: MOUNT(): Minor changes
    * Refactor it, and drop Darwin check
    
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- url2GSI.sh
+++ url2GSI.sh
@@ -134,20 +134,15 @@ DOWNLOAD()
 MOUNT()
 {
     mkdir -p "$PROJECT_DIR/working/system"
-    if [ $(uname) == Linux ]; then
-        if `sudo mount -o ro "$1" "$PROJECT_DIR/working/system" > /dev/null 2>&1`; then
-             echo '-> System image successfully mounted'
+    if `sudo mount -o ro "$1" "$PROJECT_DIR/working/system" > /dev/null 2>&1`; then
+        echo '-> System image successfully mounted'
+    elif `sudo mount -o loop "$1" "$PROJECT_DIR/working/system" > /dev/null 2>&1`; then
+            echo '-> System image successfully mounted'
         else
-             if `sudo mount -o loop "$1" "$PROJECT_DIR/working/system" > /dev/null 2>&1`; then
-                  echo '-> System image successfully mounted'
-             else
-                  # If it fails again, abort
-                  echo "-> Failed to mount image, try to check this manually"
-                  exit 1
-             fi
+            # If it fails again, abort
+            echo "-> Failed to mount image, try to check this manually"
+            exit 1
         fi
-    elif [ $(uname) == Darwin ]; then
-        fuse-ext2 "$1" "$PROJECT_DIR/working/system"
     fi
 }
 
