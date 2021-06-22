47b4ddbff4e62795cf3ada27b63c830f7c7e788b
Velosh <daffetyxd@gmail.com>
2021-06-22 17:44:52 -0300

    setup.sh: Sync installation method from VeloshGSIs/Bot3-Workflow repo
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
-    if [[ "$distro" == "arch" ]]; then
-       echo "-> Arch Linux Detected"
-       sudo pacman -S --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract dtc xz python-pip brotli lz4 gawk libmpack aria2 expect
-    else
-       echo " - Starting the installation of packages"
-       sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract build-essential device-tree-compiler liblzma-dev python-pip brotli liblz4-tool gawk aria2 rename python3-setuptools expect -y
-    fi
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "-> Darwin Detected". 
-    echo " - Starting the installation of packages"
-    brew install protobuf xz brotli lz4 aria2
+set -e
+
+if [ "${EUID}" -ne 0 ]; then
+    echo "-> Run as root!"
+    exit 1
 fi
 
-# Pip things
-pip install -U setuptools; pip3 install -U setuptools
-pip install backports.lzma protobuf pycrypto google
-pip3 install backports.lzma protobuf pycrypto google
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
+    if [[ ! "$distro" == "arch" && ! "$distro" == "darwin" ]]; then
+        echo " - Starting the installation of packages"
+        if $(sudo -E apt-get -qq install bc build-essential zip curl libstdc++6 git wget python gcc clang libssl-dev rsync flex bison ccache expect aria2 unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev brotli liblz4-tool axel gawk aria2 detox cpio rename build-essential simg2img aria2 python3-pip tree >>./.apt 2>&1); then
+            echo "-> Successfully installed."
+            rm -rf "$(pwd)/.apt"
+        else
+            echo "-> Failed, here the log:"
+            cat "$(pwd)/.apt"
+            rm -rf "$(pwd)/.apt"
+        fi
 
-sudo chmod -R +x *
-sudo chmod -R 0777 *
+        # pip moment
+        pip3 install wheel setuptools
+        pip3 install backports.lzma docopt zstandard bsdiff4 protobuf pycrypto
+        pip3 install --upgrade protobuf
+    fi
+fi
