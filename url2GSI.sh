c8f72280d5650ac85c09daa8f5b9c4576f858c25
Velosh <daffetyxd@gmail.com>
2021-06-28 15:24:58 -0300

    url2GSI.sh: Make MOUNT() say which image was mounted [1/3]
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- url2GSI.sh
+++ url2GSI.sh
@@ -136,12 +136,12 @@ MOUNT()
 {
     mkdir -p "$PROJECT_DIR/working/system"
     if `sudo mount -o ro "$1" "$PROJECT_DIR/working/system" > /dev/null 2>&1`; then
-        echo '-> System image successfully mounted'
+        echo "-> $2 image successfully mounted"
     elif `sudo mount -o loop "$1" "$PROJECT_DIR/working/system" > /dev/null 2>&1`; then
-        echo '-> System image successfully mounted'
+        echo "-> $2 image successfully mounted"
     else
         # If it fails again, abort
-        echo "-> Failed to mount image, try to check this manually"
+        echo "-> Failed to mount $2 image, try to check this manually"
         exit 1
     fi
 }
@@ -184,7 +184,7 @@ if [ $MOUNTED == false ]; then
         rm -rf "$ZIP_NAME"
     fi
     if [ -f "$PROJECT_DIR/working/system.img" ]; then
-        MOUNT "$PROJECT_DIR/working/system.img"
+        MOUNT "$PROJECT_DIR/working/system.img" "System"
         URL="$PROJECT_DIR/working/system"
     else
         echo "-> Error, system image doesn't exist!"
