ce262d1182b53bd8ad7029db22a47cdcdcb66caf
Velosh <daffetyxd@gmail.com>
2021-07-04 11:19:21 -0300

    roms: 10: ZUI: Minor changes
    * Drop old wizard
    * Edit bootprof path, eg:
    
    $ md5sum libsurfaceflinger.so
    124019840d4c20a8f443213fc8ac4df6  libsurfaceflinger.so
    $ sed -i "s|/proc/bootprof|/system/erfan/bootprof|g" libsurfaceflinger.so
    $ md5sum libsurfaceflinger.so
    2fe11aae538fff8213106b24334f8633  libsurfaceflinger.so
    
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- roms/10/ZUI/make.sh
+++ roms/10/ZUI/make.sh
@@ -3,20 +3,12 @@
 systempath=$1
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
-# AOSP libs
-#cp -fpr $thispath/lib64/* $1/lib64/
+# Try to disable bootprof
+echo "ro.bootprof.disable=1" >> $systempath/build.prop
 
-# Workaround for SetupWizard Problem
-# Remove ZUI's SetupWizard and push GoogleSetupWizard
-# Then disable SetupWizard with prop.
-# ZUI's SetupWizard out of screen on 16:9 devices.
-# These devices cannot finish setupwizard and gets stuck.
-rm -rf $1/priv-app/ZuiSetupWizard
-cp -fpr $thispath/SetupWizard $1/priv-app/
+# Copy erfan bootprof
+cp -fpr $thispath/erfan $systempath/
 
-# remove phh qtiaudio
-rm -rf $1/priv-app/QtiAudio
-rm -rf $1/app/NQNfcNci
-
-# fix notch
-sed -i "s/M -50,0 L -50,90 L 50,90 L 50,0 Z/000000000000000000000000000000000/" $1/framework/framework-res.apk
+# Edit bootprof path
+sed -i "s|/proc/bootprof|/system/erfan/bootprof|g" $systempath/lib/libsurfaceflinger.so
+sed -i "s|/proc/bootprof|/system/erfan/bootprof|g" $systempath/lib64/libsurfaceflinger.so
