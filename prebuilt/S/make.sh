5204562a1d28863bb4e583b23b1bfc4ee911fd21
Nippon <37734266+nnippon@users.noreply.github.com>
2021-07-24 18:07:41 +0300

    prebuilt: 12/S: Skip apex/capex of system_ext
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- prebuilt/S/make.sh
+++ prebuilt/S/make.sh
@@ -6,7 +6,6 @@ thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
 # Deal with non-flattened apex
 $thispath/../../scripts/apex12_extractor.sh $1/apex
-$thispath/../../scripts/apex12_extractor.sh $1/system_ext/apex
 echo "ro.apex.updatable=true" >> $1/product/etc/build.prop
 
 # Copy system files
