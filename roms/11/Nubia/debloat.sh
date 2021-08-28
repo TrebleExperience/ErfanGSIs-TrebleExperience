608c695c671e5189289652d8ad46bc51089d87c3
Velosh <daffetyxd@gmail.com>
2021-08-28 19:33:00 -0300

    roms: 11: Nubia: Initial commit
    Signed-off-by: Velosh <daffetyxd@gmail.com>

---
+++ roms/11/Nubia/debloat.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+systempath=$1
+thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
+
+# Drop useless apks
+rm -rf $1/app/AutoAgingTest
+rm -rf $1/app/DTSXULTRA
+rm -rf $1/app/FactoryTestAdvanced
+rm -rf $1/app/GoodixTest
+rm -rf $1/app/NBVirtualGameHandle
+rm -rf $1/app/NubiaFastPair
+rm -rf $1/app/Stk
+rm -rf $1/app/SystemUpdate_v1.1
+rm -rf $1/app/TP_YulorePage_v1.0.0
+rm -rf $1/app/ZNubiaEdge
+rm -rf $1/app/nubia_Browser
+rm -rf $1/app/nubia_Calendar_v1.0
+rm -rf $1/app/nubia_DeskClock_NX*
+rm -rf $1/app/nubia_DynamicWallpaper_651
+rm -rf $1/app/nubia_GameHelperModule
+rm -rf $1/app/nubia_GameHighlights
+rm -rf $1/app/nubia_GameLauncher
+rm -rf $1/app/nubia_NeoHybrid
+rm -rf $1/app/nubia_PhoneManualIntegrate
+rm -rf $1/app/nubia_neoPay
+rm -rf $1/priv-app/AOD_v*.*_*-release
+rm -rf $1/priv-app/Camera
+rm -rf $1/priv-app/NBGalleryLockScreen
+rm -rf $1/priv-app/NubiaGallery
+rm -rf $1/priv-app/NubiaVideo
+rm -rf $1/priv-app/PhotoEditor
+rm -rf $1/priv-app/ZQuickSearchBox
+rm -rf $1/priv-app/nubia_HaloVoice
+rm -rf $1/priv-app/nubia_touping
+
+# Since we've dropped AOD, time to drop AOD themes
+rm -rf $1/system_ext/media/Settings/aod
+
+# Drop some themes to save space
+rm -rf $1/system_ext/media/theme/thememanager/default_king_of_glory
+rm -rf $1/system_ext/media/theme/thememanager/default_white_mech
+
+# W-what?!
+rm -rf $1/product/priv-app/ConfigUpdater
+rm -rf $1/product/priv-app/GooglePlayServicesUpdater
+
+# Drop chinese/nubia shits.
+rm -rf $1/preset_apps/*
