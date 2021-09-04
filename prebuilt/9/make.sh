6f7432e918429c6d44a708f756f861875bd6baed
Velosh <daffetyxd@gmail.com>
2021-09-04 15:21:43 -0300

    prebuilt: 9: Also drop NODEVICEOVERLAY
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- prebuilt/9/make.sh
+++ prebuilt/9/make.sh
@@ -7,21 +7,6 @@ thispath=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # Copy system stuffs
 rsync -ra $thispath/system/ $systempath
 
-# Overlays
-if [ ! -d  $1/product ]; then
-    rm -rf $1/product
-    mkdir -p $1/product
-fi
-mkdir -p $1/product/overlay
-
-cp -fpr $thispath/nondevice_overlay/* $1/product/overlay/
-
-if [ -f $romdir/NODEVICEOVERLAY ]; then
-    echo "-> Using device specific overlays is not supported in this rom. Skipping..."
-else
-    cp -fpr $thispath/overlay/* $1/product/overlay/
-fi
-
 ## Brightness fix
 # Some systems are using custom light services, don't apply this patch on those roms
 if [ -f $romdir/DONTPATCHLIGHT ]; then
