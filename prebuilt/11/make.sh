c1b2084787ae173215dbbd29a3091e6b35270da4
Velosh <daffetyxd@gmail.com>
2021-09-05 20:00:20 -0300

    prebuilt: 11, 12: Don't delete empty selinux mapping
    * Some roms doesn't boot without it, MIUI R with VNDK 29
    
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- prebuilt/11/make.sh
+++ prebuilt/11/make.sh
@@ -17,9 +17,6 @@ cat $thispath/rw-system.add.sh >> $1/bin/rw-system.sh
 # Append file_context
 cat $thispath/file_contexts >> $1/etc/selinux/plat_file_contexts
 
-# Cleanup empty selinux mappings
-find $1/system_ext/etc/selinux/mapping/ -type f -empty -delete
-
 # Disable Codec2
 sed -i "s/android.hardware.media.c2/android.hardware.erfan.c2/g" $1/etc/vintf/manifest.xml
 rm -rf $1/etc/vintf/manifest/manifest_media_c2_software.xml
