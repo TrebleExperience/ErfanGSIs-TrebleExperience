5ba40567bd53b353cec768b621faf6ddf40ca797
Velosh <daffetyxd@gmail.com>
2021-09-05 17:57:37 -0300

    `url2GSI.sh`: Enable MIUI option
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- url2GSI.sh
+++ url2GSI.sh
@@ -189,7 +189,7 @@ if [ $MOUNTED == false ]; then
         URL="$ZIP_NAME"
     fi
     if [ "$DYNAMIC" == true ]; then
-       "$PROJECT_DIR"/dynamic.sh "$URL" --odm --product --ext --oneplus --overlays --pixel
+       "$PROJECT_DIR"/dynamic.sh "$URL" --odm --product --ext --oneplus --overlays --pixel --miui
     elif [ $DYNAMIC == false ] ; then
        "$PROJECT_DIR"/zip2img.sh "$URL" "$PROJECT_DIR/working" || exit 1
     fi
