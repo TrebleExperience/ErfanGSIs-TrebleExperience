28985e9840ead360aacc3ac13cb97af0f24d50f7
Velosh <daffetyxd@gmail.com>
2021-09-06 09:32:31 -0300

    roms: 11: ZenUI: Initial commit
    Signed-off-by: Velosh <daffetyxd@gmail.com>

---
+++ roms/11/ZenUI/build/mkdir.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Project OEM-GSI Porter by Erfan Abdi <erfangplus@gmail.com>
+# Project TrebleExperience by riansouzasantos <riansart@yahoo.com>, Hitalo <hitalo331@outlook.com> and Velosh <daffetyxd@gmail.com>
+
+systemDir="$1"
+if [ ! -d "$systemdir/xrom" ]; then
+   sudo mkdir -p "$systemdir/xrom" > /dev/null 2>&1
+   chmod 0755 "$systemdir/xrom"
+fi
+if [ ! -d "$systemdir/asdf" ]; then
+   sudo mkdir -p "$systemdir/asdf" > /dev/null 2>&1
+   chmod 0755 "$systemdir/asdf"
+fi
+if [ ! -d "$systemdir/APD" ]; then
+   sudo mkdir -p "$systemdir/APD" > /dev/null 2>&1
+   chmod 0755 "$systemdir/APD"
+fi
+if [ ! -d "$systemdir/ADF" ]; then
+   sudo mkdir -p "$systemdir/ADF" > /dev/null 2>&1
+   chmod 0755 "$systemdir/ADF"
+fi
+if [ ! -d "$systemdir/motor_fw1" ]; then
+   sudo mkdir -p "$systemdir/motor_fw1" > /dev/null 2>&1
+   chmod 0755 "$systemdir/motor_fw1"
+fi
+
+if [ ! -d "$systemdir/motor_fw2" ]; then
+   sudo mkdir -p "$systemdir/motor_fw2" > /dev/null 2>&1
+   chmod 0755 "$systemdir/motor_fw2"
+fi
