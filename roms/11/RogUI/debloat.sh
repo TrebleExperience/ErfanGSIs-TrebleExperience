6549cc9e67f505b0913c96a426b14f5ecc36c526
Velosh <daffetyxd@gmail.com>
2021-09-06 22:03:49 -0300

    roms: 11: RogUI: Update debloat list
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- roms/11/RogUI/debloat.sh
+++ roms/11/RogUI/debloat.sh
@@ -12,7 +12,6 @@ rm -rf $1/priv-app/NextApp
 rm -rf $1/priv-app/Facebook*
 rm -rf $1/priv-app/WeatherTime
 rm -rf $1/priv-app/AsusDataTransfer
-rm -rf $1/priv-app/*LiveWallpaper*
 rm -rf $1/priv-app/SmartReading
 rm -rf $1/priv-app/ROGAirTrigger
 rm -rf $1/priv-app/ROGGameCenter
@@ -20,6 +19,8 @@ rm -rf $1/app/NextAppCore
 rm -rf $1/app/Instagram
 rm -rf $1/app/Facebook*
 rm -rf $1/app/JakartaBaca
+rm -rf $1/app/TwinApps
+rm -rf $1/app/TwinAppsService
 rm -rf $1/product/app/Maps
 rm -rf $1/product/app/YouTube
 rm -rf $1/product/app/Messages
@@ -38,6 +39,15 @@ rm -rf $1/product/priv-app/AndroidAutoStub
 rm -rf $1/product/priv-app/SearchSelector
 rm -rf $1/product/priv-app/GoogleLens
 
+# Drop some themes
+rm -rf $1/etc/AsusTheme/themes/com.asus.themes.bumper.ip0206.zip
+rm -rf $1/etc/AsusTheme/themes/com.asus.themes.akira.zip
+rm -rf $1/etc/AsusTheme/themes/com.asus.themes.anakin.zip
+
+# Drop some system_ext apks
+rm -rf $1/system_ext/priv-app/EmergencyInfo
+rm -rf $1/system_ext/priv-app/GoogleFeedback
+
 # Drop some useless shits
 rm -rf $1/etc/preload/*
 
@@ -55,6 +65,7 @@ rm -rf $1/app/Facebook
 rm -rf $1/app/Netflixstubplus
 rm -rf $1/app/NetflixActivation
 rm -rf $1/app/mangaDeals
+rm -rf $1/app/slaservice
 rm -rf $1/priv-app/GameBroadcaster
 rm -rf $1/priv-app/WeatherTime
 rm -rf $1/priv-app/AsusDeskClock
@@ -79,11 +90,6 @@ rm -rf $1/priv-app/AsusEmergencyHelp
 rm -rf $1/priv-app/AsusGalleryBurst
 rm -rf $1/priv-app/AsusDialer
 rm -rf $1/priv-app/AsusContacts
-rm -rf $1/priv-app/AsusSplendid
-rm -rf $1/priv-app/AsusSplendid
-rm -rf $1/priv-app/AsusSplendid
-rm -rf $1/priv-app/AsusSplendid
-rm -rf $1/priv-app/AsusSplendid
 rm -rf $1/product/app/YouTube
 rm -rf $1/product/app/Maps
 rm -rf $1/product/app/Messages
