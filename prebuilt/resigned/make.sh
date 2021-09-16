3ee80c9edcff5b3eab58c34a1c3490e2885a81d6
Velosh <daffetyxd@gmail.com>
2021-09-16 16:10:44 -0300

    prebuilt: 10, 11, 12 & common: Fix plat_file_contexts stuff
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- prebuilt/resigned/make.sh
+++ prebuilt/resigned/make.sh
@@ -6,5 +6,8 @@ thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 # Copy system stuffs
 rsync -ra $thispath/system/ $systempath
 
+# Append file_context
+cat $thispath/file_contexts >> $1/etc/selinux/plat_file_contexts
+
 python $thispath/../../scripts/custom_manifest.py $1/../../manifest.xml $thispath/manifest.xml $1/etc/vintf/manifest.xml
 cp -fpr $1/../../manifest.xml $1/etc/vintf/manifest.xml
