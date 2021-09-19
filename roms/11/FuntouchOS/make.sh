9e78aadcc19fc0685a9eb41aad3143568d3ce3f0
Velosh <daffetyxd@gmail.com>
2021-09-19 18:13:02 -0300

    roms: 11: FuntouchOS: Merge some SGSIs patches
    * Credits to Xiaoxindada.
    
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- roms/11/FuntouchOS/make.sh
+++ roms/11/FuntouchOS/make.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-# Dummy.
+# Disable cpu platform verification
+sed -i '/ro.vivo.product.solution/d' $1/build.prop
+sed -i '/ro.vivo.product.platform/d' $1/build.prop
+
+# Resolution
+sed -i '/ro.vivo.lcm.xhd/d' $1/build.prop
