71a83d275a41f6fe0b96f4b54a62dfc15a01a738
Velosh <daffetyxd@gmail.com>
2021-09-20 12:33:36 -0300

    roms: 11: HiOS: Initial commit
    Signed-off-by: Velosh <daffetyxd@gmail.com>

---
+++ roms/11/HiOS/build/mkdir.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Project OEM-GSI Porter by Erfan Abdi <erfangplus@gmail.com>
+# Project TrebleExperience by riansouzasantos <riansart@yahoo.com>, Hitalo <hitalo331@outlook.com> and Velosh <daffetyxd@gmail.com>
+
+systemDir="$1"
+if [ ! -d "$systemdir/tranfs" ]; then
+   sudo mkdir -p "$systemdir/tranfs" > /dev/null 2>&1
+   chmod 0755 "$systemdir/tranfs"
+fi
