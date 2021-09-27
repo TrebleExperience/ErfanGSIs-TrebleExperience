218fd8e629f984ac04312d802b87efd7fa2766f7
Velosh <daffetyxd@gmail.com>
2021-09-27 08:52:17 -0300

    roms: 11: Moto: Initial commit
    * This probably need environ & jar patches
    
    Signed-off-by: Velosh <daffetyxd@gmail.com>

---
+++ roms/11/Moto/debloat.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# System Ext
+rm -rf $1/system_ext/priv-app/DemoMode
+
+# Product
+rm -rf $1/product/app/Chrome
+rm -rf $1/product/app/Duo
+rm -rf $1/product/app/GoogleOne
+rm -rf $1/product/app/GoogleTTS
+rm -rf $1/product/app/Maps
+rm -rf $1/product/app/Messages
+rm -rf $1/product/app/MotCamera2AI
+rm -rf $1/product/app/Photos
+rm -rf $1/product/app/Videos
+rm -rf $1/product/app/YTMusic
+rm -rf $1/product/app/YouTube
+rm -rf $1/product/app/facebook-appmanager
+rm -rf $1/product/app/talkback
+rm -rf $1/product/priv-app/AMXGlobalContainer
+rm -rf $1/product/priv-app/BRApps2
+rm -rf $1/product/priv-app/GoogleDialer
+rm -rf $1/product/priv-app/GuideMe
+rm -rf $1/product/priv-app/MotCamera2
+rm -rf $1/product/priv-app/MotoHelp
+rm -rf $1/product/priv-app/TelcelContainer
+rm -rf $1/product/priv-app/Turbo
+rm -rf $1/product/priv-app/Velvet
+rm -rf $1/product/priv-app/Wellbeing
