e572cc0c0f620f17a1eec58a2ce33a7696917a24
Velosh <daffetyxd@gmail.com>
2021-09-29 11:37:35 -0300

    roms: 11: Drop Rian from credits
    * He's not a founding member at TrebleExp (2nd Fundation).
    
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- roms/11/MyOS/build/mkdir.sh
+++ roms/11/MyOS/build/mkdir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Project OEM-GSI Porter by Erfan Abdi <erfangplus@gmail.com>
-# Project TrebleExperience by riansouzasantos <riansart@yahoo.com>, Hitalo <hitalo331@outlook.com> and Velosh <daffetyxd@gmail.com>
+# Project TrebleExperience by Hitalo <hitalo331@outlook.com> and Velosh <daffetyxd@gmail.com>
 
 systemDir="$1"
 if [ ! -d "$systemdir/vgc" ]; then
