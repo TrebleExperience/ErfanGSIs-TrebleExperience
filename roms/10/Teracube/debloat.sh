845eb0fc80e07077ab3b76e7eec276d025e6fffd
Velosh <daffetyxd@gmail.com>
2021-10-03 15:58:32 -0300

    roms: 10, 11: Minor changes
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- roms/10/Teracube/debloat.sh
+++ roms/10/Teracube/debloat.sh
@@ -7,9 +7,7 @@ thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 rm -rf $1/product/app/Drive
 rm -rf $1/product/app/Duo
 rm -rf $1/product/app/Videos
-rm -rf $1/product/app/WebViewGoogle
 rm -rf $1/product/app/YTMusic
 rm -rf $1/product/priv-app/FilesGoogle
 rm -rf $1/product/priv-app/GoogleDialer
 rm -rf $1/product/priv-app/GoogleFeedback
-rm -rf $1/product/priv-app/WebViewGoogle
--- roms/11/VOS/debloat.sh
+++ roms/11/VOS/debloat.sh
@@ -3,26 +3,27 @@
 systempath=$1
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
+rm -rf $1/priv-app/VsCamera
 rm -rf $1/product/app/CalculatorGoogle
 rm -rf $1/product/app/CalendarGoogle
 rm -rf $1/product/app/DeskClockGoogle
 rm -rf $1/product/app/Drive
 rm -rf $1/product/app/Duo
 rm -rf $1/product/app/Gmail2
-rm -rf $1/product/app/GoogleCalendarSyncAdapter
 rm -rf $1/product/app/GoogleAssistant
+rm -rf $1/product/app/GoogleCalendarSyncAdapter
 rm -rf $1/product/app/GoogleLocationHistory
 rm -rf $1/product/app/GoogleTTS
 rm -rf $1/product/app/Keep
 rm -rf $1/product/app/Maps
 rm -rf $1/product/app/Photos
-rm -rf $1/product/app/talkback
 rm -rf $1/product/app/Videos
 rm -rf $1/product/app/WebViewGoogle
-rm -rf $1/product/app/YouTube
 rm -rf $1/product/app/YTMusic
+rm -rf $1/product/app/YouTube
+rm -rf $1/product/app/talkback
 rm -rf $1/product/priv-app/AndroidAutoStub
-rm -rf $1/product/priv-app/Velvet
 rm -rf $1/product/priv-app/Turbo
+rm -rf $1/product/priv-app/Velvet
 rm -rf $1/product/priv-app/Wellbeing
 rm -rf $1/system_ext/priv-app/GoogleFeedback
