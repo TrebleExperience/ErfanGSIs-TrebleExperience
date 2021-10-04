89df54ca4e0dcb617fee597965868f80ff1289a3
Velosh <daffetyxd@gmail.com>
2021-10-04 15:25:45 -0300

    roms: 11: MIUI: Drop redudant code
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- roms/11/MIUI/make.sh
+++ roms/11/MIUI/make.sh
@@ -14,7 +14,6 @@ rsync -ra $thispath/system/ $systempath
 # Drop some things
 sed -i 's/<bool name="support_round_corner">true/<bool name="support_round_corner">false/' $1/etc/device_features/*
 sed -i "/ro.miui.notch/d" $1/build.prop
-sed -i "/persist.miui.density_v2/d" $1/build.prop
 
 # Clear the miui promotion context that causes almost all models to bootloop or boot directly to recovery/fastboot/bl
 sed -i '/miui.reverse.charge/d' $1/system_ext/etc/selinux/system_ext_property_contexts
