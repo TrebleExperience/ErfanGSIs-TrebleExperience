f6da6493218505d784fbd69296a77a0c6f61418b
Velosh <daffetyxd@gmail.com>
2021-10-04 15:18:18 -0300

    roms: 11: MIUI: Add missing SGSI's patch
    Co-authored-by: xiaoxindada <2245062854@qq.com>
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- roms/11/MIUI/make.sh
+++ roms/11/MIUI/make.sh
@@ -8,6 +8,9 @@ echo "ro.hardware.fp.fod=true" >> $1/build.prop
 echo "persist.vendor.sys.fp.fod.location.X_Y=445,1260" >> $1/build.prop
 echo "persist.vendor.sys.fp.fod.size.width_height=190,190" >> $1/build.prop
 
+# Copy system stuffs
+rsync -ra $thispath/system/ $systempath
+
 # Drop some things
 sed -i 's/<bool name="support_round_corner">true/<bool name="support_round_corner">false/' $1/etc/device_features/*
 sed -i "/ro.miui.notch/d" $1/build.prop
