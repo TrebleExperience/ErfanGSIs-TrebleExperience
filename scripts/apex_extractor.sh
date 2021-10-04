bb3c625ce0889ba5086217ed0b77c197934bf395
Velosh <daffetyxd@gmail.com>
2021-10-04 11:29:59 -0300

    scripts: `apex_extractor.sh`: Minor changes
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- scripts/apex_extractor.sh
+++ scripts/apex_extractor.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
-LOCALDIR=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
-EXT4EXTRACT=$LOCALDIR/../tools/ext4extract/ext4extract.py
-
-TMPDIR=$LOCALDIR/../tmp/apex_ext
-mkdir -p "$TMPDIR"
-
+# Core variables, don't touch.
+LOCALDIR=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
+DEAPEXER="$LOCALDIR/../tools/apex/deapexer.py"
+TMPDIR="$LOCALDIR/../tmp/apex_ext"
 APEXDIR="$1"
-APEXES=$(ls "$APEXDIR" | grep ".apex")
+
+# Start process
+APEXES=$(ls "$APEXDIR" | grep "\.apex$")
 for APEX in $APEXES; do
-    APEXNAME=$(echo "$APEX" | sed 's/.apex//')
-    if [[ -d "$APEXDIR/$APEXNAME" ]]; then
+    APEXNAME=$(echo "$APEX" | sed 's/\.apex//')
+    if [[ -d "$APEXDIR/$APEXNAME" || -d "$APEXDIR/$APEX" ]]; then
         continue
     fi
     mkdir -p "$APEXDIR/$APEXNAME"
-    7z e "$APEXDIR/$APEX" apex_payload.img apex_pubkey -o"$APEXDIR/$APEXNAME" 2>/dev/null >> "$TMPDIR"/zip.log
-    $EXT4EXTRACT "$APEXDIR/$APEXNAME/apex_payload.img" -D "$APEXDIR/$APEXNAME" 2>/dev/null
-    rm "$APEXDIR/$APEXNAME/apex_payload.img"
+    7z e -y "$APEXDIR/$APEX" apex_pubkey -o"$APEXDIR/$APEXNAME" >> $TMPDIR/apex_extract.log
+    $DEAPEXER extract "$APEXDIR/$APEX" "$APEXDIR/$APEXNAME"
     rm -rf "$APEXDIR/$APEXNAME/lost+found"
-    rm "$APEXDIR/$APEX"
 done
 
-rm -rf "$TMPDIR"
+# For Android 12
+CAPEXES=$(ls "$APEXDIR" | grep "\.capex$")
+for CAPEX in $CAPEXES; do
+    if [[ -z $CAPEX ]]; then
+        continue
+    fi
+    CAPEXNAME=$(echo "$CAPEX" | sed 's/\.capex//')
+    7z e -y "$APEXDIR/$CAPEX" original_apex -o"$APEXDIR" >> $TMPDIR/apex_extract.log
+    mv -f "$APEXDIR/original_apex" "$APEXDIR/$CAPEXNAME.apex"
+    mkdir -p "$APEXDIR/$CAPEXNAME"
+    7z e -y "$APEXDIR/$CAPEXNAME.apex" apex_pubkey -o"$APEXDIR/$CAPEXNAME" >> $TMPDIR/apex_extract.log
+    $DEAPEXER "$APEXDIR/$CAPEXNAME.apex" "$APEXDIR/$CAPEXNAME"
+    rm -rf "$APEXDIR/$CAPEXNAME/lost+found"
+    rm -rf "$APEXDIR/$CAPEX"
+done
