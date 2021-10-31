7b64d3e02dd59e6f617d73b22e1b1603e72639e4
Velosh <daffetyxd@gmail.com>
2021-10-31 12:56:15 -0300

    roms: 11: RealmeUI: Minor changes
    * Merge some SGSI patches
    * Minor changes
    * Others.
    
    Co-authored-by: xiaoxindada <2245062854@qq.com>
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- roms/11/RealmeUI/make.sh
+++ roms/11/RealmeUI/make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 systempath=$1
-thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
+thispath=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Copy system files
 rsync -ra $thispath/system/ $systempath
@@ -9,39 +9,94 @@ rsync -ra $thispath/system/ $systempath
 # Append file_context
 cat $thispath/file_contexts >> $1/etc/selinux/plat_file_contexts
 
-# Drop some props (If have, but make sure)
-sed -i "/ro.build.oplus_ext_partitions/d" $1/build.prop
-sed -i "/ro.oppo.my_country_root/d" $1/build.prop
-sed -i "/ro.oppo.my_engineer_root/d" $1/build.prop
-sed -i "/ro.oppo.my_engineering_root/d" $1/build.prop
-sed -i "/ro.oppo.my_manifest_root/d" $1/build.prop
-sed -i "/ro.oppo.my_odm_root/d" $1/build.prop
-sed -i "/ro.oppo.my_operator_root/d" $1/build.prop
-sed -i "/ro.oppo.my_preload_root/d" $1/build.prop
-sed -i "/ro.oppo.my_product_root/d" $1/build.prop
-sed -i "/ro.oppo.my_region_root/d" $1/build.prop
-sed -i "/ro.oppo.my_stock_root/d" $1/build.prop
-sed -i "/ro.oppo.my_version_root/d" $1/build.prop
-sed -i "/ro.oppo.oppo_custom_root/d" $1/build.prop
-sed -i "/ro.oppo.oppo_engineer_root/d" $1/build.prop
-sed -i "/ro.oppo.oppo_version_root/d" $1/build.prop
+# Fix oppo init
+sed -i 's/on boot/on early-init/g' $1/../init.oppo.rc 2> /dev/null
 
-# Re-add props
-echo "" >> $1/build.prop
-echo "# TRY TO FORCE PROPS" >> $1/build.prop
-echo "ro.oppo.my_country_root=/my_country" >> $1/build.prop
-echo "ro.oppo.my_country_root=/my_region" >> $1/build.prop
-echo "ro.oppo.my_engineer_root=/my_engineering" >> $1/build.prop
-echo "ro.oppo.my_engineering_root=/my_engineering" >> $1/build.prop
-echo "ro.oppo.my_manifest_root=/my_manifest" >> $1/build.prop
-echo "ro.oppo.my_odm_root=/odm" >> $1/build.prop
-echo "ro.oppo.my_operator_root=/my_carrier" >> $1/build.prop
-echo "ro.oppo.my_preload_root=/my_preload" >> $1/build.prop
-echo "ro.oppo.my_product_root=/my_product" >> $1/build.prop
-echo "ro.oppo.my_region_root=/my_region" >> $1/build.prop
-echo "ro.oppo.my_stock_root=/my_stock" >> $1/build.prop
-echo "ro.oppo.my_version_root=/my_version" >> $1/build.prop
-echo "ro.oppo.oppo_custom_root=/my_custom" >> $1/build.prop
-echo "ro.oppo.oppo_engineer_root=/my_engineering" >> $1/build.prop
-echo "ro.oppo.oppo_version_root=/my_version" >> $1/build.prop
-echo "ro.build.oplus_ext_partitions=my_product my_engineering my_stock my_heytap my_region my_carrier my_preload my_company" >> $1/build.prop
+# Delete redundant files/stuff
+rm -rf $1/etc/selinux/*debug*
+[[ -e $1/product/etc/selinux/mapping ]] && rm -rf $1/product/etc/selinux/*debug*
+[[ -e $1/system_ext/etc/selinux/mapping ]] && rm -rf $1/system_ext/etc/selinux/*debug*
+[[ -e $1/../my_product/vendor/firmware ]] && rm -rf $1/../my_product/vendor/firmware
+
+# Push custom bp
+echo "
+# oppo custom partition path
+ro.oppo.my_carrier_root=/my_carrier
+ro.oppo.my_company_root=/my_company
+ro.oppo.my_country_root=/my_region
+ro.oppo.my_custom_root=/my_custom
+ro.oppo.my_engineer_root=/my_engineering
+ro.oppo.my_engineering_root=/my_engineering
+ro.oppo.my_heytap_root=/my_heytap
+ro.oppo.my_manifest_root=/my_manifest
+ro.oppo.my_operator_root=/my_carrier
+ro.oppo.my_preload_root=/my_preload
+ro.oppo.my_product_root=/my_product
+ro.oppo.my_region_root=/my_region
+ro.oppo.my_special_preload_root=/special_preload
+ro.oppo.my_stock_root=/my_stock
+ro.oppo.my_version_root=/my_version
+ro.oppo.oppo_custom_root=/my_company
+ro.oppo.oppo_engineer_root=/my_engineering
+ro.oppo.oppo_product_root=/my_product
+ro.oppo.oppo_version_root=/my_version
+" >> $1/build.prop
+
+echo "
+# oppo custom partition path
+ro.oppo.my_carrier_root=/my_carrier
+ro.oppo.my_company_root=/my_company
+ro.oppo.my_country_root=/my_region
+ro.oppo.my_custom_root=/my_custom
+ro.oppo.my_engineer_root=/my_engineering
+ro.oppo.my_engineering_root=/my_engineering
+ro.oppo.my_heytap_root=/my_heytap
+ro.oppo.my_manifest_root=/my_manifest
+ro.oppo.my_operator_root=/my_carrier
+ro.oppo.my_preload_root=/my_preload
+ro.oppo.my_product_root=/my_product
+ro.oppo.my_region_root=/my_region
+ro.oppo.my_special_preload_root=/special_preload
+ro.oppo.my_stock_root=/my_stock
+ro.oppo.my_version_root=/my_version
+ro.oppo.oppo_custom_root=/my_company
+ro.oppo.oppo_engineer_root=/my_engineering
+ro.oppo.oppo_product_root=/my_product
+ro.oppo.oppo_version_root=/my_version
+" >> $1/product/build.prop
+
+# [DUMMY CODE ALERT] START
+cp -frp $1/system_ext/build.prop $thispath
+sed -i "/#end/d" $thispath/build.prop
+echo "#end" >> $thispath/build.prop
+master_date=$(cat $thispath/build.prop | grep "ro.build.master.date" | cut -d "=" -f 2)
+sed -i "/ro.build.master.date\=$master_date/,/#end/d" $thispath/build.prop
+echo "ro.build.master.date=$master_date" >> $thispath/build.prop
+cp -frp $thispath/build.prop $1/system_ext/build.prop
+rm -rf $thispath/build.prop
+
+oppo_build_name=$(find $1/../ -type f -name "build.prop")
+for oppo_builds in $oppo_build_name; do
+    oppo_builds="$(realpath $oppo_builds)"
+    if [ -e $oppo_builds ]; then
+        sed -i '/ro.sf.lcd/d' $oppo_builds
+        sed -i '/ro.display.brightness/d' $oppo_builds
+    fi
+done
+
+# import oppo custom build.prop (due custom partition path)
+if [ -e $1/../my_product/build.prop ]; then
+    echo "import /my_product/build.prop" >> $1/../my_product/build.prop
+fi
+
+# oppo custom build.prop merge
+if [ -e $1/../my_manifest/build.prop ]; then
+    sed -i "/security_patch\=/d" $1/../my_manifest/build.prop
+    echo "" >> $1/build.prop
+    cat $1/../my_manifest/build.prop >>$1/build.prop
+    true > $1/../my_manifest/build.prop
+fi
+# [DUMMY CODE ALERT] END
+
+# Run utils
+bash $thispath/util/make.sh "$(realpath $thispath/../../../working)" "$systempath"
---
+++ roms/11/RealmeUI/util/make.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Usage: <1: working folder path> <2: system tmp path>
+
+# Based off on: https://github.com/xiaoxindada/SGSI-build-tool/blob/11/make/rom_make_patch/color/make.sh
+OPARTITION="odm"
+WORKING="$1"
+SYSTEMDIR="$2"
+
+# Mount util
+MOUNT() {
+    if $(sudo mount -o loop "$1" "$2" >/dev/null 2>&1); then
+        GOOD=true
+    elif $(sudo mount -o ro "$1" "$2" >/dev/null 2>&1); then
+        GOOD=true
+    elif $(sudo mount -o ro -t erofs "$1" "$2" >/dev/null 2>&1); then
+        GOOD=true
+    elif $(sudo mount -o loop -t erofs "$1" "$2" >/dev/null 2>&1); then
+        GOOD=true
+    else
+        echo " - Failed to mount $3 image, abort!"
+        exit 1
+    fi
+}
+
+# [LOGGING]
+echo " - Merging/patching some stuff..."
+
+# Mount all images (Skipped my_*)
+for partition in $OPARTITION; do
+    if [ -f "$WORKING/$partition.img" ]; then
+        [ ! -d "$WORKING/$partition" ] && mkdir -p "$WORKING/$partition"
+        MOUNT $WORKING/$partition.img $WORKING/$partition "$partition"
+    fi
+done
+
+# Initial vars
+odmdir="$WORKING/$OPARTITION"
+framework_dir="$odmdir/framework"
+permissions_dir="$odmdir/etc/permissions"
+jar_files="$(find $framework_dir -maxdepth 1 -type f -name "*oplus*jar")"
+permissions_files="$(find $permissions_dir -maxdepth 1 -type f -name "*oplus*xml)")"
+
+# Copy oppo jars
+for oppo_jars in $jar_files; do
+    oppo_jars="$(realpath $oppo_jars)"
+    cp -frp $oppo_jars $SYSTEMDIR/system_ext/framework
+done
+
+# Copy oppo permissions
+for oppo_permissions in $permissions_files; do
+    oppo_permissions="$(realpath $oppo_permissions)"
+    cp -frp $oppo_jars $SYSTEMDIR/system_ext/etc/permissions
+done
+
+# Drop some files
+rm -rf $SYSTEMDIR/system_ext/framework/*"ufsplus"* $SYSTEMDIR/system_ext/etc/permissions/*"ufsplus"*
+
+# Copy ORMS
+core="$(realpath "$(find $odmdir -type f -name "orms_core_config.xml")")"
+permission="$(realpath "$(find $odmdir -type f -name "orms_permission_config.xml")")"
+cp -frp "$core" $SYSTEMDIR/system_ext/etc/permissions/
+cp -frp "$permission" $SYSTEMDIR/system_ext/etc/permissions/
+
+# Fix path
+permissions_files="$(find $SYSTEMDIR/system_ext/etc/permissions -type f -name "*.xml")"
+for patch_files in $permissions_files; do
+    patch_files="$(realpath $patch_files)"
+    sed -i "s#/odm/#/system/system_ext/#g" $patch_files
+done
+
+# Done!
+echo " - Done!"
